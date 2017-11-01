6e7cf8512ebd3c2ed65884ff48679da628800d61
filkate <ef.forjob@gmail.com>
2017-11-01 16:09:31 +0300

    added third step

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const print = (message) => console.log(message);
-
-export const greeting = () => {
-   print('Welcome to the Brain Games!');
-   const name = readlineSync.question('May I have your name? ');
-   print('Hello, ' + name);
-}
-
+const print = message => console.log(message);
+const greeting = () => {
+  print('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  print(`Hello, ${name}`);
+};
+export default greeting;
