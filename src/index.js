c25245354323b9450c8ade4bb1521bcfdd5304fa
filkate <ef.forjob@gmail.com>
2017-11-05 16:39:39 +0300

    replaced cycle by iterative process in game engine

--- src/index.js
+++ src/index.js
@@ -3,21 +3,26 @@ import readlineSync from 'readline-sync';
 function play(rule, step) {
   console.log('Welcome to the Brain Games!');
   console.log(rule);
+  console.log('\n');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
-  let i;
-  for (i = 0; i < 3; i += 1) {
-    const { question, answer } = step();
 
+  const isSuccess = (correctAnswers) => {
+    if (correctAnswers === 3) {
+      return true;
+    }
+    const { question, answer } = step();
     const userAnswer = readlineSync.question(`Question: ${question}\nYour answer? `);
     if (userAnswer === answer) {
       console.log('Correct!');
-    } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}.`);
-      break;
+      const newCorrectAnswers = correctAnswers + 1;
+      return isSuccess(newCorrectAnswers);
     }
-  }
-  if (i === 3) {
+    console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}.`);
+    return false;
+  };
+
+  if (isSuccess(0)) {
     console.log(`Congratulations, ${name}!`);
   } else {
     console.log(`Let's try again, ${name}!`);
